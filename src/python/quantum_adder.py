85c87b849d8b1758feea3ffe993295a93fe46030
Luca G <xxlpmghdxx@gmail.com>
2023-03-05 19:05:45 +0100

    added python code

---
+++ src/python/quantum_adder.py
@@ -0,0 +1,26 @@
+import numpy as np
+from qiskit import *
+from qiskit.providers.aer import QasmSimulator
+
+circuit = QuantumCircuit(4, 2)
+
+# init states
+circuit.x(1)
+circuit.x(2)
+
+# gates
+circuit.toffoli(0, 1, 3);
+circuit.cx(0, 1);
+circuit.toffoli(1, 2, 3);
+circuit.cx(1, 2);
+circuit.cx(0, 1);
+
+circuit.measure([2,3], [0,1]);
+
+simulator = QasmSimulator()
+compiled_circuit = transpile(circuit, simulator)
+job = simulator.run(compiled_circuit, shots=1000)
+result = job.result()
+
+counts = result.get_counts(compiled_circuit)
+print("{output : number of shots} = ",counts)
